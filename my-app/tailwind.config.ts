cdedef50748c4268e7c09ae1a06c636e1d3e3a72
go5010 <gota.4305@gmail.com>
2024-06-21 07:45:06 +0900

    flowbite導入_20240621

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -30,6 +31,6 @@ const config: Config = {
       AdSlogan: ["游ゴシック"],
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 export default config;
