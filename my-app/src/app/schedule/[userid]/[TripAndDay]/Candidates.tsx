2d3a2ae63a9fe87404bfb032257bfa71d842dfd4
go5010 <gota.4305@gmail.com>
2024-07-16 21:45:41 +0900

    20240716候補ページ作成開始，MUIインストール前

--- my-app/src/app/schedule/[userid]/[TripAndDay]/Candidates.tsx
+++ my-app/src/app/schedule/[userid]/[TripAndDay]/Candidates.tsx
@@ -2,11 +2,51 @@ import Link from "next/link";
 import React from "react";
 
 const Candidates = () => {
-  const userid = "go";
+  const userTrip = [
+    {
+      id: "trip11111",
+      title: "旅行1",
+      schedules: [
+        [
+          {
+            title: "spot1",
+            memo: "写真を撮る",
+            location: { lat: 135, lng: 40 },
+          },
+        ],
+        [{ title: "spot2", memo: "~~食べる", location: { lat: 136, lng: 41 } }],
+      ],
+    },
+    {
+      id: "trip22222",
+      title: "旅行2",
+      schedules: [
+        [
+          {
+            title: "spot3",
+            memo: "写真を撮る",
+            location: { lat: 137, lng: 40 },
+          },
+        ],
+        [{ title: "spot4", memo: "~~食べる", location: { lat: 134, lng: 41 } }],
+      ],
+    },
+  ];
+
   return (
-    <div>
-      <div>Candidates</div>
-      <Link href={"/schedule/" + userid}>aaa</Link>
+    <div className="flex mt-5 px-3">
+      <div className="w-2/3 flex flex-col">
+        <button className="border border-gray-300 shadow-md rounded-lg w-1/3 text-left px-3 py-2 mb-3">
+          首里城
+        </button>
+        <button className="border border-gray-300 shadow-md rounded-lg w-1/3 text-left px-3 py-2 mb-3">
+          首里城
+        </button>
+        <button className="border border-gray-300 shadow-md rounded-lg w-4/5 text-left px-3 py-2 mb-3">
+          首里城
+        </button>
+      </div>
+      <div>google map, 移動時間検索</div>
     </div>
   );
 };
