a9b2c2114b8b9ed97a24a413157bff8255ec9dff
go5010 <gota.4305@gmail.com>
2024-07-20 13:33:52 +0900

    20240720候補card完成?2

--- my-app/src/app/schedule/[userid]/[TripAndDay]/Candidates.tsx
+++ my-app/src/app/schedule/[userid]/[TripAndDay]/Candidates.tsx
@@ -101,43 +101,11 @@ const Candidates = () => {
     );
   };
 
+  const [targetSpot, setTargetSpot] = useState<number>();
+
   return (
     <div className="flex mt-5 px-3">
       <div className="w-2/3 flex flex-col">
-        <button className="border border-gray-300 shadow-md rounded-lg w-1/3 text-left px-3 py-2 mb-3">
-          <p className="font-semibold">首里城</p>
-        </button>
-        <button className="border border-gray-300 shadow-md rounded-lg w-1/3 text-left px-3 py-2 mb-3">
-          <p className="font-semibold">国際通り</p>
-        </button>
-        <div className="relative w-4/5">
-          <button
-            className="border border-gray-300 shadow-md rounded-lg w-full text-left px-3 py-2 mb-3"
-            onClick={(): void => alert("aaa")}
-          >
-            <p className="font-semibold">瀬長島ウミカジテラス</p>
-            <div className="mt-2">
-              <div className="flex items-center mb-1">
-                <span className="text-sm mx-1">4.4</span>
-                <Rating
-                  name="half-rating-read"
-                  defaultValue={4.4}
-                  precision={0.1}
-                  readOnly
-                  size="small"
-                />
-              </div>
-              <div>・営業日/時間</div>
-              <br />
-              <div className="mb-[75px]">・メモ</div>
-            </div>
-          </button>
-          <button className=" p-1 rounded-md hover:bg-slate-200 absolute top-1 right-1">
-            <ImShrink2 />
-          </button>
-          <textarea className="w-[calc(100%-40px)] border rounded-md py-1 px-2 outline-none resize-none absolute bottom-[30px] left-[20px]"></textarea>
-        </div>
-
         {userTrip
           .find((trip) => {
             return trip.id === urlTripID;
@@ -153,7 +121,10 @@ const Candidates = () => {
               >
                 <button
                   className="border border-gray-300 shadow-md rounded-lg w-full text-left px-3 py-2 mb-3"
-                  onClick={() => handleCardClick(spotIndex)}
+                  onClick={() => {
+                    handleCardClick(spotIndex);
+                    setTargetSpot(spotIndex);
+                  }}
                 >
                   <p className="font-semibold">{spot.title}</p>
                   {cardOpen![spotIndex].open === true && (
@@ -188,47 +159,6 @@ const Candidates = () => {
               </div>
             );
           })}
-
-        {/* {userTrip
-          .find((trip) => {
-            return trip.id === urlTripID;
-          })
-          ?.schedules[urlTripDay - 1].map((spot, spotIndex) => {
-            return (
-              <button
-                className="border border-gray-300 shadow-md rounded-lg w-1/3 text-left px-3 py-2 mb-3"
-                onClick={() => handleCardClick(spotIndex)}
-              >
-                <div className="font-semibold">{spot.title}</div>
-                {cardOpen![spotIndex].open === true && (
-                  <button
-                    className="absolute top-1 right-1 p-1 rounded-md hover:bg-slate-200"
-                    onClick={() => handleShrinkClick(spotIndex)}
-                  >
-                    <ImShrink2 />
-                  </button>
-                )}
-                {cardOpen![spotIndex].open === true && (
-                  <div className="mt-2">
-                    <div className="flex items-center mb-1">
-                      <span className="text-sm mx-1">4.4</span>
-                      <Rating
-                        name="half-rating-read"
-                        defaultValue={4.4}
-                        precision={0.1}
-                        readOnly
-                        size="small"
-                      />
-                    </div>
-                    <div>・営業日/時間</div>
-                    <br />
-                    <div className="mb-1">・メモ</div>
-                    <textarea className="w-full border rounded-md py-1 px-2 outline-none"></textarea>
-                  </div>
-                )}
-              </button>
-            );
-          })} */}
       </div>
       <div>google map, 移動時間検索</div>
     </div>
